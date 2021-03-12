ba08bddf94003d83b518b0de58dcd847e7d86332
stephSalazar <anastephany.salazarguzman@gmail.com>
2021-03-11 20:38:24 -0600

    refactor(users): Delete unused code

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,124 +1,16 @@
-// const mongoose = require('mongoose');
 const users = require('express').Router()
-
-let user = {
-  name:'ANA',
-  lastname: 'Steph'
-};
-
-let response = {
-  error: false,
-  code: 200,
-  message: ''
-};
-
+const userController = require('../controllers/usersController');
 
 // Usando users.route(), puedo agrupar distintos métodos que responden a una misma URL
-users.route('/user')
-  .get(function (req, res) {
-    response = {
-      error: false,
-      code: 200,
-      message: ''
-    };
-    
-    if(user.name === '' || user.lastname === '') {
-      response = {
-        error: true,
-        code: 501,
-        message: 'El usuario no ha sido creado'
-      };
-    } else {
-      response = {
-        error: false,
-        code: 200,
-        message: 'Respuesta del usuario, segundo intento',
-        response: user
-      };
-    }
 
-    res.send(response);
-  })
-  .post(function (req, res) {
-    if(!req.body.name || !req.body.lastname) {
-      response = {
-        error: true,
-        code: 502,
-        message: 'El campo name y lastname son requeridos'
-      };
-    } else {
-      if(user.name !== '' || user.lastname !== '') {
-        response = {
-          error: true,
-          code: 503,
-          message: 'El usuario ya fue creado previamente'
-        };
-      } else {
-        user = {
-          name: req.body.name,
-          lastname: req.body.lastname
-        };
-        response = {
-          error: false,
-          code: 200,
-          message: 'Usuario creado',
-          response: user
-        };
-      }
-    }
-  
-    res.send(response);
-  })
-  .put(function (req, res) {
-    if(!req.body.name || !req.body.lastname) {
-      response = {
-        error: true,
-        code: 502,
-        message: 'El campo name y lastname son requeridos'
-      };
-    } else {
-      if(user.name === '' || user.lastname === '') {
-        response = {
-          error: true,
-          code: 501,
-          message: 'El usuario no ha sido creado'
-        };
-      } else {
-        user = {
-          name: req.body.name,
-          lastname: req.body.lastname
-        };
-        response = {
-          error: false,
-          code: 200,
-          message: 'Usuario actualizado',
-          response: user
-        };
-      }
-    }
+users.route('/')
+  .get(userController.findAllUsers)
+  .post(userController.addUser);
 
-    res.send(response);
-  })
-  .delete(function (req, res) {
-    if(user.name === '' || user.lastname === '') {
-      response = {
-        error: true,
-        code: 501,
-        message: 'El usuario no ha sido creado'
-      };
-    } else {
-      response = {
-        error: false,
-        code: 200,
-        message: 'Usuario eliminado'
-      };
-      user = { 
-        name: '', 
-        lastname: '' 
-      };
-    }
 
-    res.send(response);
-  });
+users.route('/:id')
+  .get(userController.findById)
+  .put(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = users
